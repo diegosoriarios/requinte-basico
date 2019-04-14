02496a0e6cd62d1c4dc73369a9dfd3bc997b38af
Gabriel F <gabrielkrabbe@outlook.com>
2019-04-14 20:06:34 -0300

    adição do controle de autenticação e sistema de rotas

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const app = express()
+const jwt = require('jsonwebtoken');
 
 const path = require('path')
 const sqlite = require('sqlite')
-const dbConnection = sqlite.open(path.resolve(__dirname, 'banco.sqlite'), { Promise })
+const dbConnection = sqlite.open('./banco.sqlite', { Promise })
+
+const app = express()
 
 const port = process.env.PORT || 3000
 
-app.use('/admin', (req, res, next) =>{
-    if (req.hostname === 'localhost'){
-        next()
-    } else {
-        res.send('Not Allowed!')
-    }
-})
-
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-app.use(express.static(path.resolve(__dirname,'public')))
+app.use(express.static(path.resolve(__dirname, 'public')))
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/', async(req, res) => {
+
+app.use('/admin', require('./route/admin.js'))
+
+app.get('/', async (req, res) => {
     const db = await dbConnection
     const categoriasDb = await db.all('select * from categorias')
     const produtos = await db.all('select * from produtos')
@@ -36,7 +33,7 @@ app.get('/', async(req, res) => {
     })
 })
 
-app.get('/produto/:id', async(req, res) => {
+app.get('/produto/:id', async (req, res) => {
     const db = await dbConnection
     const produto = await db.get('select * from produtos where id = ' + req.params.id)
     res.render('produto', {
@@ -44,62 +41,13 @@ app.get('/produto/:id', async(req, res) => {
     })
 })
 
-app.get('/admin', (req, res) => {
-    res.render('admin/home')
-})
-
-app.get('/admin/produtos', async(req, res) => {
-    const db = await dbConnection
-    const produtos = await db.all('select * from produtos')
-    res.render('admin/produtos', {
-        produtos
-    })
-})
-
-app.get('/admin/produtos/delete/:id', async(req, res) => {
-    const db = await dbConnection
-    await db.run('delete from produtos where id = ' + req.params.id)
-    res.redirect('/admin/produtos')
-})
-
-app.get('/admin/novo-produto', async(req, res) => {
-    const db = await dbConnection
-    const categorias = await db.all('select * from categorias')
-    res.render('admin/novo-produto', {
-        categorias
-    })
-})
-
-app.post('/admin/novo-produto', async(req, res) => {
-    const db = await dbConnection
-    const {titulo, descricao, categoria, preco} = req.body
-    await db.run(`insert into produtos(categoria, titulo, descricao, preco) values('${categoria}', '${titulo}', '${descricao}', '${preco}')`)
-    res.redirect('/admin/produtos')
-})
-
-app.get('/admin/produtos/editar/:id', async(req, res) => {
-    const db = await dbConnection
-    const categorias = await db.all('select * from categorias')
-    const produto = await db.get('select * from produtos where id = ' + req.params.id)
-    res.render('admin/editar-produto', {
-        categorias,
-        produto
-    })
-})
-
-app.post('/admin/produtos/editar/:id', async(req, res) => {
-    const db = await dbConnection
-    const {id} = req.params
-    const {titulo, descricao, categoria, preco} = req.body
-    await db.run(`update produtos set categoria=${categoria}, titulo='${titulo}', descricao='${descricao}', preco=${preco} where id=${id}`)
-    res.redirect('/admin/produtos')
-})
-
-const init = async() => {
+const init = async () => {
     const db = await dbConnection
     await db.run('create table if not exists categorias (id INTEGER PRIMARY KEY, categoria TEXT)')
+    await db.run('create table if not exists usuarios (id INTEGER PRIMARY KEY, usuario TEXT,senha TEXT)')
     await db.run('create table if not exists produtos (id INTEGER PRIMARY KEY, categoria INTEGER, titulo TEXT, descricao TEXT, preco REAL)')
 }
+
 init()
 
 app.listen(port, (err) => {
